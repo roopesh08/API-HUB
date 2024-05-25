5538c07519b0104648adf128ad2cdc7ed326aaef
roopesh08 <roopeshvotarikari3@gmail.com>
2024-05-25 11:03:02 -0400

    Changes made in Put request

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -35,30 +35,31 @@ const setGoal = asyncHandler(async (req, res) => {
 // @route   PUT /api/goals/:id
 // @access  Private
 const updateGoal = asyncHandler(async (req, res) => {
-  const goal = await Goal.findById(req.params.id)
+  // const goal = await Goal.findById(req.params.id)
 
-  if (!goal) {
-    res.status(400)
-    throw new Error('Goal not found')
-  }
+  // if (!goal) {
+  //   res.status(400)
+  //   throw new Error('Goal not found')
+  // }
 
-  // Check for user
-  if (!req.user) {
-    res.status(401)
-    throw new Error('User not found')
-  }
+  // // Check for user
+  // if (!req.user) {
+  //   res.status(401)
+  //   throw new Error('User not found')
+  // }
 
-  // Make sure the logged in user matches the goal user
-  if (goal.user.toString() !== req.user.id) {
-    res.status(401)
-    throw new Error('User not authorized')
-  }
+  // // Make sure the logged in user matches the goal user
+  // if (goal.user.toString() !== req.user.id) {
+  //   res.status(401)
+  //   throw new Error('User not authorized')
+  // }
 
-  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  })
+  // const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+  //   new: true,
+  // })
 
-  res.status(200).json(updatedGoal)
+  // res.status(200).json(updatedGoal)
+  res.status(200).json(req.query)
 })
 
 // @desc    Delete goal
